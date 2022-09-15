9c8f484530c4db87ee45b13cc831664367d6c64c
amine <mohamine19@gmail.com>
2022-09-15 16:21:47 +0100

    trying to resolve the the second formulaa for the dashboard

--- controllers/admin/dashboard.js
+++ controllers/admin/dashboard.js
@@ -13,9 +13,21 @@ exports.getDashboard = catchAsync(async (req, res) => {
         { $match: { "store.storeId": store._id, active: true, used: true } },
         {
           $group: {
-            _id: "$category",
+            _id: {
+              category: "$category",
+            },
             count: { $sum: 1 },
           },
+        },
+        {
+          $project: {
+            _id: 0,
+            category: "$_id.category",
+            count: "$count",
+          },
+        },
+        {
+          $sort: { category: 1 },
         },
       ]);
       const selectedMaterials = await Material.find({
@@ -42,6 +54,19 @@ exports.getDashboard = catchAsync(async (req, res) => {
       const filteredSelectedStatus = selectedLastStatus.filter(
         (elem) => elem !== null
       );
+      const averageCleanliness =
+        Math.ceil(
+          (filteredSelectedStatus.length / selectedStatus.length) * 100
+        ) || 0;
+
+      let progressColor = "";
+      if (averageCleanliness <= 100 && averageCleanliness > 70) {
+        progressColor = "success";
+      } else if (averageCleanliness <= 70 && averageCleanliness > 40) {
+        progressColor = "warning";
+      } else if (averageCleanliness <= 40 && averageCleanliness >= 0) {
+        progressColor = "danger";
+      }
 
       return {
         store: store.name,
@@ -49,6 +74,8 @@ exports.getDashboard = catchAsync(async (req, res) => {
         cleanlinessFilter: {
           nbrMaterial: selectedStatus.length,
           goodMaterials: filteredSelectedStatus.length,
+          averageCleanliness,
+          progressColor,
         },
       };
     })
@@ -56,5 +83,57 @@ exports.getDashboard = catchAsync(async (req, res) => {
 
   // TODO: count how many PC's with status fixed/good on each store for categories (laptop, pc , all in one , server)
 
-  res.send(JSON.stringify(storeMaterials));
+  const storeComputerStatus = await Promise.all(
+    stores.map(async (store) => {
+      const material = await Material.aggregate([
+        // {
+        //   $match: {
+        //     "store.storeId": store._id,
+        //     active: true,
+        //     used: true,
+        //     category: { $in: ["PC", "Laptop", "Server", "All in one"] },
+        //   },
+        // },
+        {
+          $lookup: {
+            from: "Status",
+            localField: "lastStatus",
+            foreignField: "_id",
+            pipeline: [
+              { $match: { cleanliness: "dirty", physicalState: "bad" } },
+            ],
+            as: "status",
+          },
+        },
+        // {
+        //   $group: {
+        //     _id: {
+        //       name: "$name",
+        //       lastStatus: "$lastStatus",
+        //     },
+        //   },
+        // },
+        // {
+        //   $project: {
+        //     _id: 0,
+        //     name: "$_id.name",
+        //     lastStatus: "$_id.lastStatus",
+        //   },
+        // },
+      ]);
+      // console.log(material);
+
+      return { storeName: store.name, material };
+    })
+  );
+
+  // console.log(storeComputerStatus);
+
+  // res.render("admin/dashboard/dashboard", {
+  //   pageTitle: "Dashboard",
+  //   url: "/admin",
+  //   data: storeMaterials,
+  // });
+
+  res.send(JSON.stringify(storeComputerStatus));
 });
